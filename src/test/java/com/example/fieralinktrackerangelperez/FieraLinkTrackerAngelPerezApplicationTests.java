2fcd051a118611fd0dcdc568520955f4cd192dd9
Angel Perez <pasaperez@yahoo.com.ar>
2021-12-05 16:36:29 -0300

    Initial Structure of Spring Boot
    Add Swagger for API Documentation

---
+++ src/test/java/com/example/fieralinktrackerangelperez/FieraLinkTrackerAngelPerezApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fieralinktrackerangelperez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FieraLinkTrackerAngelPerezApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
